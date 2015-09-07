7b983a0d925cc62adc69130889c51f9792b88863
Conrad Petrich <d00252926@dmail.dixie.edu>
2015-09-07 14:48:05 -0600

    reverted to old comimt in order to work out bugs.

---
+++ ai-agents/build/linux/prog/ScavengerWorld/.depend-g.Snorlax.d
@@ -0,0 +1,184 @@
+00obj-g/Snorlax.o .depend-g.Snorlax.d :  \
+ /home/conrad/Desktop/CS4300/aicode/ai-agents/prog/ScavengerWorld/Snorlax.cpp \
+ /usr/include/stdc-predef.h \
+ /home/conrad/Desktop/CS4300/aicode/ai-agents/prog/ScavengerWorld/Snorlax.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/ai_scavenger.h \
+ /usr/include/c++/4.9/string \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/os_defines.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/cpu_defines.h \
+ /usr/include/c++/4.9/bits/stringfwd.h \
+ /usr/include/c++/4.9/bits/memoryfwd.h \
+ /usr/include/c++/4.9/bits/char_traits.h \
+ /usr/include/c++/4.9/bits/stl_algobase.h \
+ /usr/include/c++/4.9/bits/functexcept.h \
+ /usr/include/c++/4.9/bits/exception_defines.h \
+ /usr/include/c++/4.9/bits/cpp_type_traits.h \
+ /usr/include/c++/4.9/ext/type_traits.h \
+ /usr/include/c++/4.9/ext/numeric_traits.h \
+ /usr/include/c++/4.9/bits/stl_pair.h /usr/include/c++/4.9/bits/move.h \
+ /usr/include/c++/4.9/bits/concept_check.h \
+ /usr/include/c++/4.9/type_traits \
+ /usr/include/c++/4.9/bits/stl_iterator_base_types.h \
+ /usr/include/c++/4.9/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/4.9/debug/debug.h \
+ /usr/include/c++/4.9/bits/stl_iterator.h \
+ /usr/include/c++/4.9/bits/ptr_traits.h \
+ /usr/include/c++/4.9/bits/predefined_ops.h \
+ /usr/include/c++/4.9/bits/postypes.h /usr/include/c++/4.9/cwchar \
+ /usr/include/wchar.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.9/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.9/include/stddef.h \
+ /usr/include/xlocale.h /usr/include/c++/4.9/cstdint \
+ /usr/lib/gcc/x86_64-linux-gnu/4.9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/c++/4.9/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/c++allocator.h \
+ /usr/include/c++/4.9/ext/new_allocator.h /usr/include/c++/4.9/new \
+ /usr/include/c++/4.9/exception \
+ /usr/include/c++/4.9/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/4.9/bits/exception_ptr.h \
+ /usr/include/c++/4.9/bits/nested_exception.h \
+ /usr/include/c++/4.9/bits/localefwd.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/c++locale.h \
+ /usr/include/c++/4.9/clocale /usr/include/locale.h \
+ /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/4.9/iosfwd \
+ /usr/include/c++/4.9/cctype /usr/include/ctype.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/c++/4.9/bits/ostream_insert.h \
+ /usr/include/c++/4.9/bits/cxxabi_forced.h \
+ /usr/include/c++/4.9/bits/stl_function.h \
+ /usr/include/c++/4.9/backward/binders.h \
+ /usr/include/c++/4.9/bits/range_access.h \
+ /usr/include/c++/4.9/bits/basic_string.h \
+ /usr/include/c++/4.9/ext/atomicity.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/gthr.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/timex.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/atomic_word.h \
+ /usr/include/c++/4.9/initializer_list \
+ /usr/include/c++/4.9/ext/string_conversions.h \
+ /usr/include/c++/4.9/cstdlib /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/c++/4.9/cstdio /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/c++/4.9/cerrno /usr/include/errno.h \
+ /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/c++/4.9/bits/functional_hash.h \
+ /usr/include/c++/4.9/bits/hash_bytes.h \
+ /usr/include/c++/4.9/bits/basic_string.tcc /usr/include/c++/4.9/vector \
+ /usr/include/c++/4.9/bits/stl_construct.h \
+ /usr/include/c++/4.9/ext/alloc_traits.h \
+ /usr/include/c++/4.9/bits/alloc_traits.h \
+ /usr/include/c++/4.9/bits/stl_uninitialized.h \
+ /usr/include/c++/4.9/bits/stl_vector.h \
+ /usr/include/c++/4.9/bits/stl_bvector.h \
+ /usr/include/c++/4.9/bits/vector.tcc /usr/include/c++/4.9/map \
+ /usr/include/c++/4.9/bits/stl_tree.h \
+ /usr/include/c++/4.9/ext/aligned_buffer.h \
+ /usr/include/c++/4.9/bits/stl_map.h /usr/include/c++/4.9/tuple \
+ /usr/include/c++/4.9/utility /usr/include/c++/4.9/bits/stl_relops.h \
+ /usr/include/c++/4.9/array /usr/include/c++/4.9/stdexcept \
+ /usr/include/c++/4.9/bits/uses_allocator.h \
+ /usr/include/c++/4.9/bits/stl_multimap.h /usr/include/c++/4.9/thread \
+ /usr/include/c++/4.9/chrono /usr/include/c++/4.9/ratio \
+ /usr/include/c++/4.9/limits /usr/include/c++/4.9/ctime \
+ /usr/include/c++/4.9/bits/parse_numbers.h \
+ /usr/include/c++/4.9/functional /usr/include/c++/4.9/typeinfo \
+ /usr/include/c++/4.9/memory /usr/include/c++/4.9/bits/stl_tempbuf.h \
+ /usr/include/c++/4.9/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/4.9/ext/concurrence.h \
+ /usr/include/c++/4.9/bits/unique_ptr.h \
+ /usr/include/c++/4.9/bits/shared_ptr.h \
+ /usr/include/c++/4.9/bits/shared_ptr_base.h \
+ /usr/include/c++/4.9/backward/auto_ptr.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/ai_xy.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/ai_agent.h \
+ /usr/include/c++/4.9/iostream /usr/include/c++/4.9/ostream \
+ /usr/include/c++/4.9/ios /usr/include/c++/4.9/bits/ios_base.h \
+ /usr/include/c++/4.9/bits/locale_classes.h \
+ /usr/include/c++/4.9/bits/locale_classes.tcc \
+ /usr/include/c++/4.9/streambuf /usr/include/c++/4.9/bits/streambuf.tcc \
+ /usr/include/c++/4.9/bits/basic_ios.h \
+ /usr/include/c++/4.9/bits/locale_facets.h /usr/include/c++/4.9/cwctype \
+ /usr/include/wctype.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/ctype_base.h \
+ /usr/include/c++/4.9/bits/streambuf_iterator.h \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/ctype_inline.h \
+ /usr/include/c++/4.9/bits/locale_facets.tcc \
+ /usr/include/c++/4.9/bits/basic_ios.tcc \
+ /usr/include/c++/4.9/bits/ostream.tcc /usr/include/c++/4.9/istream \
+ /usr/include/c++/4.9/bits/istream.tcc /usr/include/c++/4.9/sstream \
+ /usr/include/c++/4.9/bits/sstream.tcc /usr/include/c++/4.9/cstring \
+ /usr/include/string.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Options.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Socket.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Message.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/RandomNumber.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Action.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/PerceptAtom.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Percept.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/AgentProgram.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Location.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Object.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Agent.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Environment.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/EnvironmentDisplay.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/AgentDriver.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/EnvironmentDriver.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/DisplayDriver.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Support/logging.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/XY/XYAction.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/XY/XYLocation.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/XY/XYObject.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/XY/XYAgent.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/XY/XYEnvironment.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/XY/XYEnvironmentDisplay.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerLocation.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerAction.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerCellInterface.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerObject.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerAgent.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerBase.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerCell.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerEnvironment.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerDisplayDriver.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerTexture.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/ai_glut.h \
+ /usr/include/GL/freeglut.h /usr/include/GL/freeglut_std.h \
+ /usr/include/GL/gl.h /usr/include/GL/glext.h /usr/include/inttypes.h \
+ /usr/include/GL/glu.h /usr/include/GL/freeglut_ext.h \
+ /usr/include/GL/freeglut_ext.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerModel.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerEnvironmentModel.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerHover.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerEnvironmentDisplay.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerEnvironmentDisplay3D.h \
+ /usr/include/c++/4.9/mutex /usr/include/c++/4.9/system_error \
+ /usr/include/x86_64-linux-gnu/c++/4.9/bits/error_constants.h \
+ /home/conrad/Desktop/CS4300/aicode/ai/include/Agent/Scavenger/ScavengerEnvironmentDriver.h
+00obj-g/Snorlax.o:
+	$(CXX) $(CXXFLAGS) -c $(LOCAL_SRC_DIR)/Snorlax.cpp -o $@
